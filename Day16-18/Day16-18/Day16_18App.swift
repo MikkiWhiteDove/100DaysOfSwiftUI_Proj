e13386f46d4fd0ebad1a3c1d2e41d735ff3344ae
Mishana <mishana@MacBook-Air-Mishana.local>
2022-02-09 22:43:17 +0300

    first

---
+++ Day16-18/Day16-18/Day16_18App.swift
@@ -0,0 +1,17 @@
+//
+//  Day16_18App.swift
+//  Day16-18
+//
+//  Created by Mishana on 08.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Day16_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day16-18/Day16-18/SwiftUIViewTests.swift
@@ -0,0 +1,78 @@
+//
+//  SwiftUIViewTests.swift
+//  Day16-18
+//
+//  Created by Mishana on 08.02.2022.
+//
+
+import SwiftUI
+
+//struct SwiftUIViewTests: View {
+//    let students = ["Marry", "Jack", "Anna"]
+//    @State private var selectedStudent = "Marry"
+//    var body: some View {
+//        NavigationView {
+//            Form{
+//                Picker("Select your student", selection:
+//                        $selectedStudent) {
+//                    ForEach(students, id: \.self) {
+//                        Text($0)
+//                    }
+//                }
+//
+//            }
+//        }
+//    }
+//}
+
+
+
+    //            ForEach(0..<100) {                                  // number in
+    //                Text("Number \($0)")
+    ////                Text("Number \(number)")
+
+    //struct SwiftUIViewTests: View {
+    //    @State private var name = ""
+    //    var body: some View {
+    //        Form{
+    //            TextField("Enter your name", text: $name)
+    //            Text("Your name is \(name)")
+    //        }
+    //    }
+    //}
+
+
+    //struct SwiftUIViewTests: View {
+    //    @State private var topCount = 0
+    //    var body: some View {
+    //        Button("Count = \(topCount)") {
+    //            topCount += 1
+    //        }
+    //    }
+    //}
+
+
+
+    //struct SwiftUIViewTests: View {
+    //    var body: some View {
+    //        NavigationView{
+    //            Form{
+    //                Section{
+    //                    Text("Heqlloqq,q world!")
+    //                }
+    //            }
+    //            .navigationTitle("SwiftUI")
+    //            .navigationBarTitleDisplayMode(.inline)
+    //        }
+    //
+    //    }
+    //
+    //}
+    //
+
+
+//struct SwiftUIViewTests_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SwiftUIViewTests()
+//    }
+//}
